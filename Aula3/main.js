b8e29d62e760fedfd4ff5c42039237d368164e2c
Guto Xavier <gutoffline@gmail.com>
2019-05-25 13:27:49 -0300

    Aula3

---
+++ Aula3/main.js
@@ -0,0 +1,7 @@
+function CalcularTotalProduto(){
+    alert("Aqui vamos calcular o preço total do produto");
+}
+
+function AdicionarAoCarrinho(){
+    alert("Aqui vamos adicionar o produto ao carrinho");
+}
